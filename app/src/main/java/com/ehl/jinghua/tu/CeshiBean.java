21cb6e25b47f908a5e528c63d36e295cddd3a129
shaipxiang <shaipxiang@163.com>
2016-09-20 15:15:05 +0800

    添加测试bean

---
+++ app/src/main/java/com/ehl/jinghua/tu/CeshiBean.java
@@ -0,0 +1,28 @@
+package com.ehl.jinghua.tu;
+
+/**
+ * author: jinghua
+ * created on: 2016/9/20 15:12
+ * description:
+ */
+public class CeshiBean {
+
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
